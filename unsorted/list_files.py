5f28e550e8c911d30ba130719701643d58eac84b
David Ketchum <dgketchum@gmail.com>
2016-09-08 11:00:49 -0600

    those scripts were so bad I deleted most of them

--- unsorted/list_files.py
+++
@@ -1,8 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-mypath = "C://Recharge_GIS//fdr_all.gdb"
-onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
-print str(onlyfiles)
-##
-##fileList = listdir("C:\Recharge_GIS\fdr_all.gdb")
-##print str(fileList)
